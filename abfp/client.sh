30d44f0983d7c6aeac8485d3aa5636cb935e1b8d
Albert Faro <albert.faro@enti.cat>
2021-02-04 20:40:05 +0100

    Archivos actualizados

--- abfp/client.sh
+++ abfp/client.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+IP_LOCAL="127.0.0.1"
+
+IP_SERVER="127.0.0.1"
+PORT=2021
 
 echo "Cliente de ABFP"
+
+echo "(2) Sending Headers"
+
+echo "ABFP $IP_LOCAL" | nc -q 1 $IP_SERVER $PORT
+
+echo "(3)Listening $PORT"
+
+
+
+RESPONSE = `nc -l -p $PORT`
+
+if ["$RESPONSE" != "OK_CONN"]; then
+	echo "Server offline"
+	exit 1
+fi
+
+echo "(6) HANDSHAKE"
+
+sleep 1
+
+echo "THIS_IS_MY_CLASSROOM" | nc -q 1 $IP_SERVER $PORT
+
+exit 0
--- abfp/server.sh
+++ abfp/server.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-echo "Server ABFP"
+PORT = 2021
 
-echo "Listening 2021"
+echo "(0) Server ABFP"
 
-nc -l -p 2021
+echo "(1) Listening $PORT"
+
+HEADER = `nc -l -p $PORT`
+
+echo "ABFP TESTING $HEADER"
+
+PREFIX = `echo $HEADER | cut -d " " -f 1`
+IP_CLIENT = `echo $HEADER | cut -d " " -f 2`
+
+echo "(4) RESPONSE"
+
+if ["$PREFIX" != "ABFP"]; then
+
+echo "ERROR IN HEADER!"
+
+sleep 1
+
+echo "KO_CONN" | nc -q 1 $IP_CLIENT $PORT
+
+exit 1
+
+fi
+
+sleep 1
+echo "OK_CONN" | nc -q 1 $IP_CLIENT $PORT
+
+echo "(5) LISTEN"
+
+HANDSHAKE = `nc -l -p $PORT`
+
+exit 0
